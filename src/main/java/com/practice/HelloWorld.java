025f3df8a249e6d66734ec5b08b6572e13a68020
Nitesh Singh Kataria <kataria.nitesh14@gmail.com>
2020-05-26 21:18:08 +0700

    Delete HelloWorld.java

--- src/main/java/com/practice/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.practice;
-
-import com.practice.interviewBit.FormatSpecifier;
-
-public class HelloWorld {
-    public static void main(String [] args){
-       FormatSpecifier.valueOf("HARISH").hello();
-    }
-}
