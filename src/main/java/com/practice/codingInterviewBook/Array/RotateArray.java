5dfbee3b628be19aad05985d512762d67357a30a
Nitesh Singh Kataria <nitesh.kataria@agoda.com>
2020-05-31 17:12:33 +0700

    Max Sum Contigous Sub Array 'Asked in Google, Paypal, Yahoo, Goldman Sachs.'

--- src/main/java/com/practice/codingInterviewBook/Array/RotateArray.java
+++ src/main/java/com/practice/codingInterviewBook/Array/RotateArray.java
@@ -6,7 +6,8 @@ public class RotateArray {
 
     //n =7 and k=3
     //array = [1,2,3,4,5,6,7] target=[5,6,7,1,2,3,4]
-    public ArrayList<Integer> rotateArray(ArrayList<Integer> array,Integer length, Integer rotationPoint){
+    static ArrayList<Integer> rotateArray(ArrayList<Integer> array,Integer length, Integer rotationPoint){
+//        int[] arr = new int[]
         ArrayList<Integer> targetArray = new ArrayList<Integer>();
         for(int i = length-rotationPoint; i< length;i++){
             targetArray.add(array.get(i));
@@ -23,6 +24,7 @@ public class RotateArray {
     }
 
     public static void main(String [] args){
+
 
         RotateArray rotateArray = new RotateArray();
         ArrayList<Integer> array = new ArrayList<Integer>();
--- src/main/java/com/practice/interviewBit/MaxSumContiguousSubarray.java
+++ src/main/java/com/practice/interviewBit/MaxSumContiguousSubarray.java
@@ -9,9 +9,16 @@ public class MaxSumContiguousSubarray {
         int[] arr = {-89, -277, -475, -480, -420};
         System.out.println(maxSubArray(arr));
     }
+
+    /**
+     * This  method is used to find the maximum sum of non-negative integers.
+     * @param A
+     * @return
+     */
     static int maxSubArray(int[] A) {
         int max_so_long = 0;
         int max_ending_here = 0;
+        //Check for All Negative elements in an Array.
         if (checkOnlyNegative(A) == true) {
             max_so_long = A[0];
             if (A.length == 1) {
@@ -20,10 +27,13 @@ public class MaxSumContiguousSubarray {
             for (int i = 1; i < A.length; i++) {
                 if(max_so_long < A[i]){
                     max_so_long = A[i];
+
                 }
             }
             return max_so_long;
         }
+        // This is basic Kadane's Algorithm.
+        // Sum up all the positive integer until it is not getting any negative integer in an array.
         for (int i = 0; i < A.length; i++) {
             max_ending_here = max_ending_here + A[i];
             if (max_ending_here < 0) {
@@ -36,17 +46,24 @@ public class MaxSumContiguousSubarray {
         }
         return max_so_long;
     }
-    static boolean checkOnlyNegative(int []A){
-        int checkNegative = 0;
-        for(int i =0; i<A.length;i++){
-            if(A[i]<0){
-                checkNegative = checkNegative+1;
-            }
 
+    /**
+     * This method will check for all negative in an array.
+     * because for the finding of MaxContigousSubArray is different in non-negative integers.
+     * @param A
+     * @return
+     */
+    static boolean checkOnlyNegative(int []A){
+            int checkNegative = 0;
+            for (int i = 0; i < A.length; i++) {
+                if (A[i] < 0) {
+                    checkNegative = checkNegative + 1;
+                }
+            }
+            if (checkNegative == A.length) {
+                return true;
+            }
+            return false;
         }
-        if(checkNegative == A.length){
-            return true;
-        }
-        return false;
-    }
+
 }
